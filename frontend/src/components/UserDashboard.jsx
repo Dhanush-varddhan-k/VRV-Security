294f2d6084c92ddfbf834c218a56ee531e63ee74
Dhanush <k.varddhan2002@gmail.com>
2024-11-28 22:40:19 +0530

    ui2

--- frontend/src/components/UserDashboard.jsx
+++ frontend/src/components/UserDashboard.jsx
@@ -2,10 +2,14 @@ import React from 'react';
 
 const UserDashboard = () => {
     return (
-        <div className="text-center mt-10">
+        <div className="min-h-screen bg-cover bg-center flex flex-col justify-center items-center">
+            <div className="text-center mt-10">
             <h2 className="text-2xl font-bold">User Dashboard</h2>
             <p>Welcome to the user area!</p>
         </div>
+
+        </div>
+        
     );
 };
 
